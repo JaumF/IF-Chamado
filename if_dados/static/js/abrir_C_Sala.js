74c93f6b6ab1e6beccbf5b42767aa83ea55d6db9
JaumF <netjoaof1@gmail.com>
2024-08-29 07:48:26 -0300

    isso..

---
+++ if_dados/static/js/abrir_C_Sala.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var departamentoInput = document.querySelector('input[name="departamento"]');
+    var salaSelect = document.getElementById('sala');
+
+    function updateSalaOptions(departamento) {
+        let options = [];
+
+        if (departamento === 'Laboratório') {
+            options = [
+                { value: 'sem-opcao', text: '' },
+                { value: 'Laboratório 1', text: 'Laboratório 1' },
+                { value: 'Laboratório 2', text: 'Laboratório 2' }
+            ];
+        } else if (departamento === 'Pavilhão Novo') {
+            options = [
+                { value: 'sem-opcao', text: '' },
+                { value: 'sala1', text: 'Sala 1' },
+                { value: 'sala2', text: 'Sala 2' }
+            ];
+        } else {
+            options = [
+                { value: 'sem-opcao', text: '' }
+            ];
+        }
+
+        salaSelect.innerHTML = '';
+        options.forEach(function(option) {
+            var opt = document.createElement('option');
+            opt.value = option.value;
+            opt.textContent = option.text;
+            salaSelect.appendChild(opt);
+        });
+    }
+
+    updateSalaOptions(departamentoInput.value);
+
+    departamentoInput.addEventListener('change', function() {
+        updateSalaOptions(this.value);
+    });
+});
